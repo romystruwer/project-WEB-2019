c9cd62e80860533de686adab7fe05203d0a5d164
Romy S <romystruwer@live.nl>
2020-04-02 12:16:44 +0200

    comment aangepast

--- js/liken.js
+++ js/liken.js
@@ -7,7 +7,7 @@
 //de knop selecteren
 var button = document.querySelector("button");
 
-//als je op de button klikt komt de div tevoorschijn!
+//als je op de button klikt kiest hij class = .clicked
 button.addEventListener("click", function() {
     element.classList.toggle("clicked");
 });
